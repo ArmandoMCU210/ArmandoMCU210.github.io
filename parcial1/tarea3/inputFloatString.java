787f0929a8908ff51b81761ccac7cd9f9e8b2081
ArmandoMCU210 <ArmandoMCU210@outlook.com>
2022-10-28 08:53:13 -0500

    Add files via Visual

--- parcial1/tarea3/inputFloatString.java
+++ parcial1/tarea3/inputFloatString.java
@@ -5,22 +5,22 @@ public class inputFloatString {
 class Input {
     public static void main(String[] args) {
     	
-        Scanner input = new Scanner(System.in);
-    	
-        // Getting float input
-        System.out.print("Enter float: ");
-        float myFloat = input.nextFloat();
-        System.out.println("Float entered = " + myFloat);
-    	
-        // Getting double input
-        System.out.print("Enter double: ");
-        double myDouble = input.nextDouble();
-        System.out.println("Double entered = " + myDouble);
-    	
-        // Getting String input
-        System.out.print("Enter text: ");
-        String myString = input.next();
-        System.out.println("Text entered = " + myString);
+        try (Scanner num = new Scanner(System.in)) {
+            // Getting float input
+            System.out.print("Enter float: ");
+            float myFloat = num.nextFloat();
+            System.out.println("Float entered = " + myFloat);
+            
+            // Getting double input
+            System.out.print("Enter double: ");
+            double myDouble = num.nextDouble();
+            System.out.println("Double entered = " + myDouble);
+            
+            // Getting String input
+            System.out.print("Enter text: ");
+            String myString = num.next();
+            System.out.println("Text entered = " + myString);
+        }
     }
 }
 
