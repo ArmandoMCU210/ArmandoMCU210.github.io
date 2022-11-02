4808f8b815e0d3c8d19b540e918f5a6d64033415
ArmandoMCU210 <ArmandoMCU210@outlook.com>
2022-11-01 19:04:13 -0600

    Add files via Visual

--- parcial1/tarea4/Cubica.java
+++ parcial1/tarea4/Cubica.java
@@ -3,12 +3,24 @@ package parcial1.tarea4;
 public class Cubica {
     public static void main(String args[]){
 
-        double n = 10;
+        double [] array = new double [21];
 
-        for (double i = -10; i <= n; i=i+1){
-            System.out.println(Math.pow(i, 3));
+        int n = -10;
+        int n1 = -10;
+
+        for (int i = 0; i < array.length; i++){
+            array [i] = n1;
+            n1 ++;
         }
 
+        System.out.println("---------------------");
+        
+        for (int i = 0; i < array.length; i++){
+            array[i] = Math.pow(array[i], 3);
+            System.out.println(n+" | "+array[i]);
+            n++;
+        }
 
+        System.out.println("---------------------");
     }
 }
--- parcial1/tarea4/Triangulo.java
+++ parcial1/tarea4/Triangulo.java
@@ -11,7 +11,7 @@ public class Triangulo {
         piramide.close();
 
         System.out.println();
-        for (int i = 1; i < n; i++ ){
+        for (int i = 1; i < n+1; i++ ){
             for (int j = 1; j < n; j++){
                 System.out.print(" ");
             }
