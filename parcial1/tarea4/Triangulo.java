3138aabb5dfd2efa5c07ff04c1a216514eb7b9db
ArmandoMCU210 <ArmandoMCU210@outlook.com>
2022-11-08 17:23:07 -0600

    Add files via Visual

--- parcial1/tarea4/Triangulo.java
+++ parcial1/tarea4/Triangulo.java
@@ -12,10 +12,6 @@ public class Triangulo {
 
         System.out.println();
         for (int i = 1; i < n+1; i++ ){
-            for (int j = 1; j < n; j++){
-                System.out.print(" ");
-            }
-
             for (int l = 9; l >= (i); l=l-1){
                 System.out.print(" ");
             }
