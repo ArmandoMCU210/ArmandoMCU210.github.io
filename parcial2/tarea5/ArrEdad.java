8418f4781a9ced98a910a17ea9f3396afcd2e699
ArmandoMCU210 <ArmandoMCU210@outlook.com>
2022-11-14 18:08:58 -0600

    Add files via Visual

---
+++ parcial2/tarea5/ArrEdad.java
@@ -0,0 +1,30 @@
+package parcial2.tarea5;
+
+public class ArrEdad {
+    public static void main (String arg [] ){
+
+        int [] age = new int [10];
+
+        age [0] = 12;
+        age [1] = 4;  
+        age [2] = 5;  
+        age [3] = 80;  
+        age [4] = 18;
+        age [5] = 16;
+        age [6] = 8;
+        age [7] = 98;
+        age [8] = 48;
+        age [9] = 15;
+        
+        System.out.println("array pos 0 : " + age [0]);
+        System.out.println("array pos 1 : " + age [1]); 
+        System.out.println("array pos 2 : " + age [2]); 
+        System.out.println("array pos 3 : " + age [3]); 
+        System.out.println("array pos 4 : " + age [4]);
+        System.out.println("array pos 5 : " + age [5]); 
+        System.out.println("array pos 6 : " + age [6]); 
+        System.out.println("array pos 7 : " + age [7]); 
+        System.out.println("array pos 8 : " + age [8]); 
+        System.out.println("array pos 9 : " + age [9]); 
+    }
+}
